b2613f179b5cfc7dc9752815781c2a3ff1742671
Celivra <52xiaozhai@gmail.com>
2025-04-27 20:51:43 +0800

    wordDAO insert word

--- src/main/java/com/celivra/WordDAO.java
+++ src/main/java/com/celivra/WordDAO.java
@@ -8,7 +8,15 @@ import java.util.List;
 
 public class WordDAO {
     Connection connection = DBUtil.getConnection();
-    public void insertWord(Word word) {}
+    public void insertWord(Word word) {
+        try(PreparedStatement ps = connection.prepareStatement("insert into word(english, chinese) values(?, ?)")) {
+            ps.setString(1, word.getEnglish());
+            ps.setString(2, word.getChinese());
+            ResultSet rs = ps.executeQuery();
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
     public List<Word> getAllWords() {
         List<Word> words = new ArrayList<>();
         try(PreparedStatement ps = connection.prepareStatement("select * from word")){

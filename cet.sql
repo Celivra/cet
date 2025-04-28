588a757fb4c30c07c203ae732cbe75018ef81972
Celivra <52xiaozhai@gmail.com>
2025-04-28 13:39:48 +0800

    add main function

---
+++ cet.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 9.2.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: cet
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `word`
+--
+
+DROP TABLE IF EXISTS `word`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `word` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `english` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `chinese` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `word`
+--
+
+LOCK TABLES `word` WRITE;
+/*!40000 ALTER TABLE `word` DISABLE KEYS */;
+INSERT INTO `word` VALUES (1,'actually','實際上'),(2,'deteriorate','惡化,品質下降'),(3,'rarely','很少,難得'),(4,'approximately','大概'),(5,'equivalent','相同的,相等的'),(6,'sample','樣本'),(7,'assume','以為,想當然的認為'),(8,'journey','旅程,旅行'),(9,'undermine','暗中破壞,在...下挖坑道'),(10,'component','n.組件,a.組成的');
+/*!40000 ALTER TABLE `word` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-28 13:31:32
--- src/main/java/com/celivra/Main.java
+++ src/main/java/com/celivra/Main.java
@@ -1,6 +1,7 @@
 package com.celivra;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     static List<Word> words = WordService.getWords();
@@ -10,6 +11,20 @@ public class Main {
             getRandomWord();
         }
     }
+    public static boolean test(Word word){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("="+word.getChinese()+"=");
+        System.out.println("Enter English word: ");
+        String ans= scanner.nextLine();
+        if(!ans.equals(word.getEnglish())){
+            System.out.println("Wrong English word");
+            System.out.println("correct: "+word.getEnglish());
+            scanner.nextLine();
+            return false;
+        }
+        return true;
+
+    }
     public static void getRandomWord(){
         int randomIndex;
         do{
@@ -17,8 +32,9 @@ public class Main {
         } while(words.get(randomIndex) == null);
 
         Word word = words.get(randomIndex);
-        words.set(randomIndex, null);
-        System.out.println(word);
+
+        if(test(word)) words.set(randomIndex, null);
+
     }
     public static int index(){
         random = new Random();
